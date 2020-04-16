39ccc33f71f376bc34fc8d8f183c7d0cbfb6e991
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-04-16 18:40:29 +0200

    Update texture.fs

--- build/res/texture.fs
+++ build/res/texture.fs
@@ -1,1 +1,13 @@
-a
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+    //interpolate entre les 2 textures
+    FragColor = texture(texture1, TexCoord);
+}
